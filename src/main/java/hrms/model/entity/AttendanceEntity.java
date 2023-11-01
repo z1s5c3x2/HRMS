34d7ee242af4b08c9242da3cdb0d8f365d1ea70f
junhee0125 <minervajun@gmail.com>
2023-11-01 10:27:29 +0900

    AttendanceDto/AttendanceEntity 필드명 수정

--- src/main/java/hrms/model/entity/AttendanceEntity.java
+++ src/main/java/hrms/model/entity/AttendanceEntity.java
@@ -21,23 +21,23 @@ public class AttendanceEntity extends BaseTime{
     //필드설계
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //auto-increment,
-    private int attd_no;       // 근태기록번호(식별)
+    private int attdNo;       // 근태기록번호(식별)
 
     @Column()
-    private String attd_wrst;         // 출근시간
+    private String attdWrst;         // 출근시간
     @Column()
-    private String attd_wrend;           // 퇴근 시간
+    private String attdWrend;           // 퇴근 시간
     @ToString.Exclude
-    @JoinColumn(name="emp_no")
+    @JoinColumn(name="empNo")
     @ManyToOne
-    private EmployeeEntity emp_no;              // 사원번호
+    private EmployeeEntity empNo;              // 사원번호
 
     public AttendanceDto allToDto() {
         return AttendanceDto.builder()
-                .attd_no(this.attd_no)
-                .attd_wrst(this.attd_wrst)
-                .attd_wrend(this.attd_wrend)
-                .emp_no(this.emp_no)
+                .attdNo(this.attdNo)
+                .attdWrst(this.attdWrst)
+                .attdWrend(this.attdWrend)
+                .empNo(this.empNo)
                 .build();
     }
 
