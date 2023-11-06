1d4649d37eb8f9055c7bafd5a4c999ed9b6fcb80
z1s5c3x2 <z1s5c3x2@gmail.com>
2023-11-06 16:58:20 +0900

    11 06
    리액트 스프링 서버 연동3

--- src/main/java/hrms/config/SpringMvcViewConfig.java
+++ src/main/java/hrms/config/SpringMvcViewConfig.java
@@ -1,8 +1,9 @@
 package hrms.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
+@Configuration
 public class SpringMvcViewConfig extends WebMvcConfigurerAdapter {
 
     @Override
