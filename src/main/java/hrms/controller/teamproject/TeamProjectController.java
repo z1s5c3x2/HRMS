fb9cbcc08d65bc615e61c733cc6dca9b09ab0b9f
choi ou seon <dkrtpsxm12@naver.com>
2023-11-06 18:48:06 +0900

    의선s commit

--- src/main/java/hrms/controller/teamproject/TeamProjectController.java
+++ src/main/java/hrms/controller/teamproject/TeamProjectController.java
@@ -1,8 +1,12 @@
 package hrms.controller.teamproject;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hrms.model.dto.ApprovalRequestDto;
 import hrms.model.dto.EmployeeDto;
 import hrms.model.dto.ProjectDto;
+import hrms.model.dto.TeamMemberDto;
+import hrms.service.approval.ApprovalService;
 import hrms.service.teamproject.TeamProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +19,9 @@ public class TeamProjectController {
 
     @Autowired
     private TeamProjectService teamProjectService;
+
+    @Autowired
+    private ApprovalService approvalService;
 
     // 1. 팀프로젝트 생성
     @PostMapping("/post")
@@ -34,7 +41,6 @@ public class TeamProjectController {
         return teamProjectService.getPermitAllTeamProject(approval);
     }
 
-
     // 3. 개별 팀프로젝트 출력
     @GetMapping("/getOne")
     public ProjectDto getOneTeamProject(@RequestParam int pjtNo){
@@ -42,6 +48,25 @@ public class TeamProjectController {
     }
 
     // 4. 팀프로젝트 수정
+    @PostMapping("/putAproval")
+    public boolean putAproval(@RequestBody ApprovalRequestDto<ProjectDto> approvalRequestDto) throws JsonProcessingException {
+
+        // DTO객체 => json 문자열
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(approvalRequestDto.getData());
+        approvalRequestDto.setAprvJson(json);
+
+        // 결재 테이블 등록 메서드
+        // => 실행 후 실행결과 반환
+        boolean result = approvalService.updateApproval(
+                approvalRequestDto.getAprvType(),   // 결재타입 [메모장 참고]
+                approvalRequestDto.getAprvCont(),   // 결재내용
+                approvalRequestDto.getApprovers(),  // 검토자
+                approvalRequestDto.getAprvJson()    // 수정할 JSON 문자열
+        );
+
+        return false;
+    }
 
     // 5. 팀프로젝트 삭제
 
