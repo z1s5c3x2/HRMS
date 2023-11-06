d281c68d688923d45a4a2eabe6db91bc84bfefa8
choi ou seon <dkrtpsxm12@naver.com>
2023-11-06 16:59:09 +0900

    의선s commit

--- src/main/reactapp/src/component/teamProject/TeamProjectList.js
+++ src/main/reactapp/src/component/teamProject/TeamProjectList.js
@@ -8,8 +8,20 @@ export default function TeamProjectList(props){
     // 컴포넌트 상태변수 관리
     let [rows, setRows] = useState( [] )
 
+    useEffect( () => {
+        axios
+            .get('/teamproject/getAll')
+            .then(r => {
+                setRows(r.data);    // 응답받은 값들을 상태변수에 저장
+            })
+    }, []);
+
 
     return(<>
+        <div>
+            <div>
+            </div>
+        </div>
 
     </>)
 }
