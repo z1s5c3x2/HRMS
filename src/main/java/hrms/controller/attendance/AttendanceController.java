88f8a9a2fab54d666bb8a12f53a2f4b41750fad3
junhee0125 <minervajun@gmail.com>
2023-11-08 10:25:07 +0900

    파일이동 및 css(scrollbox)수정

---
+++ src/main/java/hrms/controller/attendance/AttendanceController.java
@@ -0,0 +1,47 @@
+package hrms.controller.attendance;
+
+import hrms.model.dto.AttendanceDto;
+import hrms.service.attendance.AttendanceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/attendence")
+public class AttendanceController {
+
+    @Autowired
+    private AttendanceService attendanceService;
+
+    //출근처리
+    @PostMapping("/")
+    public boolean setAttendanceGoWork(@RequestBody AttendanceDto attendDto) {
+        return false;
+    }
+
+    //근태출력 - 달력에 표시할 모든사원 or 사원하나
+    @GetMapping("/")
+    public boolean getAttendanceListAll(){
+        return false;
+    }
+
+
+    //출결조회 - 출근지각/결근/ 모든사원 or 사원하나
+    @GetMapping("/")
+    public boolean getAttendanceInfo(){
+        return false;
+    }
+
+    //근무시간조회 - 모든사원 or 한명의 사원
+    @GetMapping("/")
+    public boolean getWorkTime(){
+        return false;
+    }
+    
+    //퇴근처리
+    @PutMapping("/")
+    public boolean setAttendanceLeaveWork(@RequestBody AttendanceDto attendDto) {
+        return false;
+    }  //휴가 /병가 등록 하나의 사원
+
+
+}
