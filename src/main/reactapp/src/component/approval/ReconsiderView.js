c31c77dd1bad0d4a8d4dab56d8b94683b7c35366
USER <hwanhee603562@gmail.com>
2023-11-12 22:15:04 +0900

    2023-11-12 리액트 상신내역 출력시 타입변경 및 결재내역 Controller 매핑

--- src/main/reactapp/src/component/approval/ReconsiderView.js
+++ src/main/reactapp/src/component/approval/ReconsiderView.js
@@ -11,7 +11,7 @@ export default function ReconsiderView(props){
     // 상신내역 요청
     useEffect( () => {
 
-        axios.get( '/approval/getApprovalHistory' )
+        axios.get( '/approval/getReconsiderHistory' )
             .then( result => {
                 console.log( result.data );
                 setReconsiderList( result.data );
@@ -19,15 +19,40 @@ export default function ReconsiderView(props){
 
     }, [])
 
-    /*const getApprovalType = r => {
-        reconsiderList
-    }*/
+    // 결재 타입에 따른 결재의 유형출력
+    const getTypeName = (aprvType) => {
 
+        switch( aprvType ){
 
-    /*
-    var str = 'HTML,CSS,JavaScript';
-    var words = str.split(',');
-    */
+            case 1: return (<>신규 사원등록</>)
+            case 2: return (<>사원정보 수정</>)
+            case 3: return (<>퇴사처리</>)
+            case 4: return (<>사원 부서이동</>)
+            case 5: return (<>사원 직급변경</>)
+            case 6: return (<>사원 휴직등록</>)
+            case 7: return (<>사원 휴직기간/지급유형 변경</>)
+            case 8: return (<>사원 병가신청</>)
+            case 9: return (<>사원 병가기간/지급유형 변경</>)
+            case 10: return (<>사원 연차신청</>)
+            case 11: return (<>사원 연차기간/지급유형 변경</>)
+            case 12: return (<>신규 프로젝트 등록</>)
+            case 13: return (<>프로젝트 수정</>)
+            case 14: return (<>프로젝트 삭제</>)
+            case 15: return (<>프로젝트팀 투입</>)
+            case 16: return (<>프로젝트팀 투입/종료일 수정</>)
+            case 17: return (<>프로젝트팀원 삭제</>)
+            case 18: return (<>급여 기본금 지급</>)
+            case 19: return (<>급여 정기상여금 지급</>)
+            case 20: return (<>급여 특별상여금 지급</>)
+            case 21: return (<>급여 성과금 지급</>)
+            case 22: return (<>급여 명절휴가비 지급</>)
+            case 23: return (<>급여 퇴직금 지급</>)
+            case 24: return (<>급여 경조사비 지급</>)
+            case 25: return (<>급여 연가보상비 지급</>)
+
+        }
+
+    }
 
 
     return (<>
@@ -46,7 +71,7 @@ export default function ReconsiderView(props){
             { reconsiderList.map( r =>(
                 <tr>
                     <td> 제 {r.apState}호 </td>
-                    <td> {r.aprvType} </td>
+                    <td> { getTypeName( r.aprvType ) } </td>
                     <td> {r.aprvCont} </td>
                     <td>
                         <span> { (r.cdate.split("T"))[0] } </span>
