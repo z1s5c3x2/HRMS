814636ac93b555fe84898ab44760a99ed423cfda
wjdgnsckck <wjdgnsckck@nate.com>
2023-11-13 21:46:16 +0900

    2023-11-13 리액트/자바 나의 결재 목록 출력 기능 완료 ※ 현재 UserDetails 완료 후 트랜잭션 수행 가능

--- src/main/reactapp/src/component/approval/ApprovalView.js
+++ src/main/reactapp/src/component/approval/ApprovalView.js
@@ -1,7 +1,8 @@
-/*import axios from 'axios';
+import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import { getTypeName } from './ListOutputConverter';
 
 export default function ApprovalMain(props){
 
@@ -16,13 +17,55 @@ export default function ApprovalMain(props){
             setApprovalList( result.data )
 
         })
-    })
+    }, [])
+
+
+
+    // 검토완료
+    const approvate = ( aprvNo, aplogSta ) => {
+
+        axios.put( '/approval/approbate', { aprvNo:aprvNo, aplogSta:aplogSta } )
+            .then( result => {
+                console.log( result.data );
+        })
+    }
 
     return (<>
 
         <h3> 결재관리 </h3>
 
+        <table className="approvalTable">
+
+            <tr>
+                <th> 결재번호 </th>
+                <th> 상신유형 </th>
+                <th> 내용 </th>
+                <th> 상신일시 </th>
+                <th> 진행상태 </th>
+            </tr>
+            { approvalList.map( r =>(
+                <tr>
+                    <td> 제 {r.apState}호 </td>
+                    <td> { getTypeName( r.aprvType ) } </td>
+                    <td> {r.aprvCont} </td>
+                    <td>
+                        <span> { (r.cdate.split("T"))[0] } </span>
+                        <span> { ((r.cdate.split("T"))[1].split("."))[0].substring(0, 5) } </span>
+                    </td>
+                    <td> {
+                        r.apState == 1 ? <> 결재완료 </>
+                        :  r.apState == 2 ? <> 반려 </>
+                        : <>
+                            <button onClick={ () => approvate( r.aprvNo, 1 ) } type="button"> 결재 </button>
+                            <button onClick={ () => approvate( r.aprvNo, 2 ) } type="button"> 반려 </button>
+                         </>
+                    }
+                    </td>
+                </tr>
+            ))}
+
+        </table>
 
     </>)
 
-}*/
+}
---
+++ src/main/reactapp/src/component/approval/ListOutputConverter.js
@@ -0,0 +1,37 @@
+// Approval 출력 시 공통 사용 메소드
+
+
+// 타입에 따른 결재의 유형 변환
+export const getTypeName = (aprvType) => {
+
+    switch( aprvType ){
+
+        case 1: return (<>신규 사원등록</>)
+        case 2: return (<>사원정보 수정</>)
+        case 3: return (<>퇴사처리</>)
+        case 4: return (<>사원 부서이동</>)
+        case 5: return (<>사원 직급변경</>)
+        case 6: return (<>사원 휴직등록</>)
+        case 7: return (<>사원 휴직기간/지급유형 변경</>)
+        case 8: return (<>사원 병가신청</>)
+        case 9: return (<>사원 병가기간/지급유형 변경</>)
+        case 10: return (<>사원 연차신청</>)
+        case 11: return (<>사원 연차기간/지급유형 변경</>)
+        case 12: return (<>신규 프로젝트 등록</>)
+        case 13: return (<>프로젝트 수정</>)
+        case 14: return (<>프로젝트 삭제</>)
+        case 15: return (<>프로젝트팀 투입</>)
+        case 16: return (<>프로젝트팀 투입/종료일 수정</>)
+        case 17: return (<>프로젝트팀원 삭제</>)
+        case 18: return (<>급여 기본금 지급</>)
+        case 19: return (<>급여 정기상여금 지급</>)
+        case 20: return (<>급여 특별상여금 지급</>)
+        case 21: return (<>급여 성과금 지급</>)
+        case 22: return (<>급여 명절휴가비 지급</>)
+        case 23: return (<>급여 퇴직금 지급</>)
+        case 24: return (<>급여 경조사비 지급</>)
+        case 25: return (<>급여 연가보상비 지급</>)
+
+    }
+
+}
