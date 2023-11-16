39436668f1762629bbde75685e5015c9187c2760
USER <hwanhee603562@gmail.com>
2023-11-17 01:29:47 +0900

    2023-11-17 리액트_전사원결재조회/개별 상신목록 조회/개별 결재목록 조회 css 완료

--- src/main/reactapp/src/component/approval/ApprovalList.js
+++ src/main/reactapp/src/component/approval/ApprovalList.js
@@ -1,3 +1,5 @@
+import styles from '../../css/approval.css';
+
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
@@ -25,23 +27,18 @@ export default function ApprovalList(props){
         page : 1 ,
         key : '' ,      // 결재번호[aprvNo], 내용[aprvCont] , 상신자명[empName]
         keyword : '',   // key에 대한 탐색명
-        aprvType : 0,
-        apState : '',
-        cdate : ''
+        aprvType : 0,   // 결재의 유형
+        apState : '',   // 결재 진행현황 [1:완료  2:반려  3:검토중]
+        cdate : ''      // 상신일자
     });
 
-    // 2. 페이지 번호를 클릭했을 때
+    // 페이지 번호를 클릭했을 때
     const onPageSelect = (e, value) =>{
 
         pageInfo.page = value;          // 클릭한 페이지번호로 변경
         setPageInfo( {...pageInfo} )    // 새로고침 [상태변수의 주소값이 바뀌면 재랜더링]
 
     }
-
-
-
-
-
 
 
     // 상신내역 요청
@@ -58,63 +55,75 @@ export default function ApprovalList(props){
     return (<>
 
         <h3> 결재 메인페이지 </h3>
+        <div class="contentBox">
+            <div class="searchBox">
 
+                <span>
+                    상신일자
+                    조회기간 : <input type="date" className="periodStart" /> ~  <input type="date" className="periodEnd" />
+                </span>
 
-        <div class="searchBox">
-            <div>
-                <select>
-                    <option value="aprvNo">
-                    <option value="aprvCont">
-                    <option value="empName">
-                </select>
-                <input type="text" />
-            <div>
+                <span>
+                    결재상태
+                    <select>
+                        <option value="1"> 완료 </option>
+                        <option value="2"> 반려 </option>
+                        <option value="3"> 검토중 </option>
+                    </select>
+                </span>
 
-            <div>
-                결재유형
-                <select>
-                    <option value="1"> test </option>
-
-                </select>
+                <span>
+                    <select>
+                        <option value="aprvNo"> 결재번호 </option>
+                        <option value="aprvCont"> 내용 </option>
+                        <option value="empName"> 상신자 </option>
+                    </select>
+                    <input type="text" />
+                    <button type="button" className=""> 검색 </button>
+                </span>
 
             </div>
 
-        </div>
 
+            <hr class="hr01"/>
+            <table className="tableTypeA">
 
-        <table className="reconsiderTable">
-
-            <tr>
-                <th> 결재번호 </th>
-                <th> 상신유형 </th>
-                <th> 내용 </th>
-                <th> 상신일시 </th>
-                <th> 상신자 </th>
-                <th> 진행상태 </th>
-            </tr>
-            { allApprovalList.map( r =>(
                 <tr>
-                    <td> 제 { r.apState }호 </td>
-                    <td> { getTypeName( r.aprvType ) } </td>
-                    <td> { r.aprvCont } </td>
-                    <td>
-                        <span> { (r.cdate.split("T"))[0] } </span>
-                        <span> { ((r.cdate.split("T"))[1].split("."))[0].substring(0, 5) } </span>
-                    </td>
-                    <td> { r.empName } </td>
-                    <td> {
-                        r.apState == 1 ? <> 결재완료 </> :  r.apState == 2 ? <> 반려 </> : <> 검토중 </>
-                    }
-                    </td>
+                    <th width="12%"> 결재번호 </th>
+                    <th width="20%"> 상신유형 </th>
+                    <th width="29%"> 내용 </th>
+                    <th width="17%"> 상신일시 </th>
+                    <th width="10%"> 상신자 </th>
+                    <th width="12%"> 진행상태 </th>
                 </tr>
-            ))}
 
-        </table>
 
-        <div style={{ display : 'flex' , flexDirection : 'column' , alignItems : 'center' , margin : '10px' }}>
+                { allApprovalList.map( r =>(
+                    <tr className="outputTd">
+                        <td> 제 { r.aprvNo }호 </td>
+                        <td> { getTypeName( r.aprvType ) } </td>
+                        <td> { r.aprvCont } </td>
+                        <td>
+                            <span> { (r.cdate.split("T"))[0] } </span>
+                            <span> { ((r.cdate.split("T"))[1].split("."))[0].substring(0, 5) } </span>
+                        </td>
+                        <td> { r.empName } </td>
+                        <td>
+                            {
+                                r.apState == 1 ? <> 결재완료 </> :  r.apState == 2 ? <> 반려 </> : <> 검토중 </>
+                            }
+                        </td>
+                    </tr>
+                ))}
 
-            {/* count : 전체 페이지 수   onChange : 페이지번호를 클릭/변경 했을 때 이벤트 */}
-            <Pagination page={ pageInfo.page } count={ 5 } variant="outlined" onChange={ onPageSelect } />
+            </table>
+
+            <div style={{ display : 'flex' , flexDirection : 'column' , alignItems : 'center' , margin : '10px' }}>
+
+                {/* count : 전체 페이지 수   onChange : 페이지번호를 클릭/변경 했을 때 이벤트 */}
+                <Pagination page={ pageInfo.page } count={ 5 } variant="outlined" onChange={ onPageSelect } />
+
+            </div>
 
         </div>
 
--- src/main/reactapp/src/component/approval/ApprovalView.js
+++ src/main/reactapp/src/component/approval/ApprovalView.js
@@ -1,8 +1,22 @@
+
+
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { getTypeName } from './ListOutputConverter';
+
+/* MUI TABLE 관련 컴포넌트 import */
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+/* MUI 페이지네이션 */
+import Pagination from '@mui/material/Pagination';
 
 export default function ApprovalMain(props){
 
@@ -15,6 +29,24 @@ export default function ApprovalMain(props){
     // 출력할 결재 리스트 상태변수
     const [ approvalList, setApprovalList ] = useState( [] )
     const [ approbateResult, setApprobateResult ] = useState( false )
+
+    // 페이징처리 및 검색필터;
+    const [pageInfo, setPageInfo] = useState({
+        page : 1 ,
+        key : '' ,      // 결재번호[aprvNo], 내용[aprvCont] , 상신자명[empName]
+        keyword : '',   // key에 대한 탐색명
+        aprvType : 0,   // 결재의 유형
+        apState : '',   // 결재 진행현황 [1:완료  2:반려  3:검토중]
+        cdate : ''      // 상신일자
+    });
+
+    // 페이지 번호를 클릭했을 때
+    const onPageSelect = (e, value) =>{
+
+        pageInfo.page = value;          // 클릭한 페이지번호로 변경
+        setPageInfo( {...pageInfo} )    // 새로고침 [상태변수의 주소값이 바뀌면 재랜더링]
+
+    }
 
     // 결재 리스트 요청
     const getApprovalList = e => {
@@ -49,39 +81,81 @@ export default function ApprovalMain(props){
     return (<>
 
         <h3> 결재관리 </h3>
+        <div class="contentBox">
+            <div class="searchBox">
 
-        <table className="approvalTable">
+                <span>
+                    상신일자
+                    조회기간 : <input type="date" className="periodStart" /> ~  <input type="date" className="periodEnd" />
+                </span>
 
-            <tr>
-                <th> 결재번호 </th>
-                <th> 상신유형 </th>
-                <th> 내용 </th>
-                <th> 상신일시 </th>
-                <th> 진행상태 </th>
-            </tr>
-            { approvalList.map( r =>(
+                <span>
+                    결재상태
+                    <select>
+                        <option value="1"> 완료 </option>
+                        <option value="2"> 반려 </option>
+                        <option value="3"> 검토중 </option>
+                    </select>
+                </span>
+
+                <span>
+                    <select>
+                        <option value="aprvNo"> 결재번호 </option>
+                        <option value="aprvCont"> 내용 </option>
+                        <option value="empName"> 상신자 </option>
+                    </select>
+                    <input type="text" />
+                    <button type="button" className=""> 검색 </button>
+                </span>
+
+            </div>
+
+            <hr class="hr01"/>
+            <table className="tableTypeA">
+
                 <tr>
-                    <td> 제 { r.aprvNo }호 </td>
-                    <td> { getTypeName( r.aprvType ) } </td>
-                    <td> { r.aprvCont !== "" ? r.aprvCont : "-" } </td>
-                    <td>
-                        <span> { (r.cdate.split("T"))[0] } </span>
-                        <span> { ((r.cdate.split("T"))[1].split("."))[0].substring(0, 5) } </span>
-                    </td>
-                    <td> {
-                        r.apState == 1 ? <> 결재완료 </>
-                        :  r.apState == 2 ? <> 반려 </>
-                        : <>
-                            <button onClick={ () => approvate( r.aprvNo, 1 ) } type="button"> 결재 </button>
-                            <button onClick={ () => approvate( r.aprvNo, 2 ) } type="button"> 반려 </button>
-                         </>
-                    }
-                    </td>
+                    <th width="12%"> 결재번호 </th>
+                    <th width="20%"> 상신유형 </th>
+                    <th width="29%"> 내용 </th>
+                    <th width="17%"> 상신일시 </th>
+                    <th width="10%"> 상신자 </th>
+                    <th width="12%"> 진행상태 </th>
                 </tr>
-            ))}
 
-        </table>
 
+                { approvalList.map( r =>(
+                    <tr className="outputTd">
+                        <td> 제 { r.aprvNo }호 </td>
+                        <td> { getTypeName( r.aprvType ) } </td>
+                        <td> { r.aprvCont !== "" ? r.aprvCont : "-" } </td>
+                        <td>
+                            <span> { (r.cdate.split("T"))[0] } </span>
+                            <span> { ((r.cdate.split("T"))[1].split("."))[0].substring(0, 5) } </span>
+                        </td>
+                        <td> { r.empName } </td>
+                        <td> {
+                            r.apState == 1 ? <> 결재완료 </>
+                            :  r.apState == 2 ? <> 반려 </>
+                            : <>
+                                <button onClick={ () => approvate( r.aprvNo, 1 ) } type="button"> 결재 </button>
+                                <button onClick={ () => approvate( r.aprvNo, 2 ) } type="button"> 반려 </button>
+                             </>
+                        }
+                        </td>
+                    </tr>
+                ))}
+
+            </table>
+
+            <div style={{ display : 'flex' , flexDirection : 'column' , alignItems : 'center' , margin : '10px' }}>
+
+                {/* count : 전체 페이지 수   onChange : 페이지번호를 클릭/변경 했을 때 이벤트 */}
+                <Pagination page={ pageInfo.page } count={ 5 } variant="outlined" onChange={ onPageSelect } />
+
+            </div>
+
+
+        </div>
 
     </>)
 
