d12dfc90227716567146eec7a82a5cf844a27101
zecace <minervajun81@gmail.com>
2023-11-19 09:45:02 +0900

    Aside/ index Routh수정

--- src/main/java/hrms/service/attendance/AttendanceService.java
+++ src/main/java/hrms/service/attendance/AttendanceService.java
@@ -6,6 +6,7 @@ import hrms.model.entity.AttendanceEntity;
 import hrms.model.entity.EmployeeEntity;
 import hrms.model.repository.AttendanceEntityRepository;
 import hrms.model.repository.EmployeeEntityRepository;
+import hrms.service.security.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,6 +23,7 @@ public class AttendanceService {
 
     @Autowired private AttendanceEntityRepository attendanceRepository;
     @Autowired private EmployeeEntityRepository employeeEntityRepository;
+    @Autowired private SecurityService securityService;
     @Transactional
     public boolean setAttendanceGoWork(AttendanceDto attendDto) {
         Optional<EmployeeEntity> employeeEntity = employeeEntityRepository.findByEmpNo(attendDto.getEmpNo());
@@ -34,14 +36,9 @@ public class AttendanceService {
     @Transactional
     public boolean setAttendanceLeaveWork(@RequestBody AttendanceDto attendDto) {
         // pk(empNo)와  오늘날짜의 출근정보 검색
+        Optional<EmployeeEntity> optionalEmployeeEntity = employeeEntityRepository.findByEmpNo(securityService.getEmp().getEmpNo());
 
-        // Optional<AttendanceEntity> attendEntity = AttendanceEntityRepository.findById(attendDto.getEmpNo());
-        //결과가 있으면 퇴근정보 update
 
-        
-        //결과 없으면  false반환
-        
-        // 해당 attpNo에
         return false;
         
     }
