69aa88bb7cc9bb099bd0c8a7e5c1739d26c0a149
USER <hwanhee603562@gmail.com>
2023-11-24 12:02:48 +0900

    2023-11-24 결재 상세페이지 value 출력 추후 CSS작업 요망

--- src/main/reactapp/src/component/approval/ApprovalDetailed.js
+++ src/main/reactapp/src/component/approval/ApprovalDetailed.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import { getTypeName } from './ListOutputConverter';
 import styles from '../../css/ApprovalDetailed.css';
 
 export default function ApprovalDetailed( { isOpen, closeModal, aprvNo } ){
+
+    // 사원 직급
+    const rkList = ["대기","사원","주임","대리","과장","팀장","부장"]
 
     // 상세 결제건 저장 상태변수
     const [moreInformation, setMoreInformation] = useState({});
@@ -20,6 +24,7 @@ export default function ApprovalDetailed( { isOpen, closeModal, aprvNo } ){
 
         axios.get( '/approval/getDetailedApproval', { params: { aprvNo: aprvNo } } )
             .then( result => {
+                console.log( result.data )
                 setMoreInformation( result.data );
         })
 
@@ -31,9 +36,39 @@ export default function ApprovalDetailed( { isOpen, closeModal, aprvNo } ){
 
             <div className="modal-overlay" onClick={closeModal}>
                 <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                    <h2>Modal Content</h2>
-                    <p>This is the content of the modal.</p>
-                    <button onClick={closeModal}>Close Modal</button>
+                    <div className="close-button">
+                        <button onClick={closeModal}> x </button>
+                    </div>
+
+                    <h3> 정보 </h3>
+                    <div className="basicApprovalInfoBox">
+                        <div> 결재번호 <span> 제 { moreInformation.aprvNo }호 </span> </div>
+                        <div> 결재유형 <span> { getTypeName( moreInformation.aprvType ) } </span> </div>
+                        <div> 진행상태
+                            <span>
+                                {
+                                    moreInformation.apState == 1 ? <> 결재완료 </> : moreInformation.apState == 2 ? <> 반려 </> : <> 검토중 </>
+                                }
+                            </span>
+                        </div>
+                        <div> 상신자   <span> {rkList[moreInformation.empRk]} </span><span> {moreInformation.empName} </span> </div>
+                        <div> 상신일시
+                            {moreInformation.cdate && (
+                                <>
+                                    <span> {(moreInformation.cdate.split("T"))[0]} </span>
+                                    <span> {((moreInformation.cdate.split("T"))[1].split("."))[0].substring(0, 5)} </span>
+                                </>
+                            )}
+                         </div>
+                    </div>
+
+                    <h3> 내용 </h3>
+                    <div className="detailedContentBox">
+                        <div> 내용 </div>
+                    </div>
+
+                    <h3> 상세진행현황 </h3>
+                    <div></div>
                 </div>
             </div>
 
